289d6035dc64cd5e8f18c7f2e8179dc22043262f
dilanrinconhbtn <937@holbertonschool.com>
2019-09-26 22:36:32 +0000

    functions

--- functions2.c
+++ functions2.c
@@ -51,46 +51,48 @@ void sub(stack_t **stack, unsigned int num_linea)
 
 void divi(stack_t **stack, unsigned int num_linea)
 {
-  stack_t *ojo;
-  stack_t *ojo2;
-  int div = 0;
+	stack_t *ojo;
+	stack_t *ojo2;
+	int div = 0;
 
-  if (*stack && (*stack)->next)
-    {
-      if ((*stack)->n == 0)
+	if (*stack && (*stack)->next)
 	{
-	  dprintf(2, "L%u: division by zero\n", num_linea);
-	  free(hola.linea);
-	  fclose(hola.fil);
-	  free_l(stack);
-	  exit(EXIT_FAILURE);
+		if ((*stack)->n == 0)
+		{
+			dprintf(2, "L%u: division by zero\n", num_linea);
+			free(hola.linea);
+			fclose(hola.fil);
+			free_l(stack);
+			exit(EXIT_FAILURE);
+		}
+		ojo2 = (*stack)->next;
+		div = ojo2->n / (*stack)->n;
+		ojo = *stack;
+		*stack = (*stack)->next;
+		if (*stack)
+			(*stack)->prev = NULL;
+		free(ojo);
+		(*stack)->n = div;
 	}
-      ojo2 = (*stack)->next;
-      div = ojo2->n / (*stack)->n;
-      ojo = *stack;
-      *stack = (*stack)->next;
-      if (*stack)
-	(*stack)->prev = NULL;
-      free(ojo);
-      (*stack)->n = div;
-    }
-  else
-    {
-      dprintf(2, "L%u: can't div, stack too short\n", num_linea);
-      free(hola.linea);
-      fclose(hola.fil);
-      free_l(stack);
-      exit(EXIT_FAILURE);
-    }
+	else
+	{
+		dprintf(2, "L%u: can't div, stack too short\n", num_linea);
+		free(hola.linea);
+		fclose(hola.fil);
+		free_l(stack);
+		exit(EXIT_FAILURE);
+	}
 }
+
+
 void free_l(stack_t **stack)
 {
-  stack_t *cleaner = *stack;
- 
-  while (*stack)
-    {
-      cleaner = cleaner->next;
-      free(*stack);
-      *stack = cleaner;
-    }
-}     
+	stack_t *cleaner = *stack;
+
+	while (*stack)
+	{
+		cleaner = cleaner->next;
+		free(*stack);
+		*stack = cleaner;
+	}
+}
