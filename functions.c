062d22434002193fd599605a3ce6f122fd7e0677
diegovivas <dieguitovivas@hotmail.com>
2019-09-26 22:06:07 +0000

    asdf

--- functions.c
+++ functions.c
@@ -37,21 +37,32 @@ void swap(stack_t **stack, unsigned int num_linea)
  * @stack: head
  * Return: void function
  */
-void add(stack_t **stack, __attribute__((unused)) unsigned int num_linea)
+void add(stack_t **stack, unsigned int num_linea)
 {
 	stack_t *ojo;
-	stack_t *ojo2 = (*stack)->next;
+	stack_t *ojo2;
 	int suma = 0;
 
-	if (!((*stack)->next))
-		printf("Errorrrr\n");
-	suma = (*stack)->n + ojo2->n;
-	ojo = *stack;
-	*stack = (*stack)->next;
-	if (*stack)
-		(*stack)->prev = NULL;
-	free(ojo);
-	(*stack)->n = suma;
+	if (*stack && (*stack)->next)
+	  {
+	    
+	    ojo2 = (*stack)->next;
+	    suma = (*stack)->n + ojo2->n;
+	    ojo = *stack;
+	    *stack = (*stack)->next;
+	    if (*stack)
+	      (*stack)->prev = NULL;
+	    free(ojo);
+	    (*stack)->n = suma;
+	  }
+	else
+	  {
+	    dprintf(2,"L%d: can't add, stack too short\n", num_linea);
+            free(hola.linea);
+            fclose(hola.fil);
+            free_l(stack);
+            exit(EXIT_FAILURE);
+	  }
 }
 
 
