dbe28380507a67fe72b00e2ae582d91ed042ad9f
dilanrinconhbtn <937@holbertonschool.com>
2019-09-26 22:34:12 +0000

    functions

--- functions2.c
+++ functions2.c
@@ -24,30 +24,31 @@ void nop(stack_t **stack, unsigned int num_linea)
 }
 void sub(stack_t **stack, unsigned int num_linea)
 {
-  stack_t *ojo;
-  stack_t *ojo2;
-  int resta = 0;
+	stack_t *ojo;
+	stack_t *ojo2;
+	int resta = 0;
 
-  if (*stack && (*stack)->next)
-    {
-      ojo2 = (*stack)->next;
-      resta = ojo2->n - (*stack)->n;
-      ojo = *stack;
-      *stack = (*stack)->next;
-      if (*stack)
-	(*stack)->prev = NULL;
-      free(ojo);
-      (*stack)->n = resta;
-    }
-  else
-    {
-    dprintf(2, "L%u: can't sub, stack too short\n", num_linea);
-    free(hola.linea);
-    fclose(hola.fil);
-    free_l(stack);
-    exit(EXIT_FAILURE);
-    }
+	if (*stack && (*stack)->next)
+	{
+		ojo2 = (*stack)->next;
+		resta = ojo2->n - (*stack)->n;
+		ojo = *stack;
+		*stack = (*stack)->next;
+		if (*stack)
+			(*stack)->prev = NULL;
+		free(ojo);
+		(*stack)->n = resta;
+	}
+	else
+	{
+		dprintf(2, "L%u: can't sub, stack too short\n", num_linea);
+		free(hola.linea);
+		fclose(hola.fil);
+		free_l(stack);
+		exit(EXIT_FAILURE);
+	}
 }
+
 void divi(stack_t **stack, unsigned int num_linea)
 {
   stack_t *ojo;
