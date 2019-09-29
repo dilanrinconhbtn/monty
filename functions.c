8e31d2551880ca488fecd83775758271591b6add
diegovivas <dieguitovivas@hotmail.com>
2019-09-29 15:21:02 +0000

    :

--- functions.c
+++ functions.c
@@ -83,7 +83,7 @@ void pint(stack_t **stack, unsigned int num_linea)
 		free(hola.linea);
 		fclose(hola.fil);
 		free_l(stack);
-		dprintf(2, "L%u can't pint, stack empty\n", num_linea);
+		dprintf(2, "L%u: can't pint, stack empty\n", num_linea);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", ojo->n);
