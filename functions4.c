19acc70249cb082cc974c6c2d14033e5176b9e0f
diegovivas <dieguitovivas@hotmail.com>
2019-09-29 15:41:18 +0000

    fix

--- functions4.c
+++ functions4.c
@@ -32,18 +32,21 @@ void rotl(stack_t **stack, unsigned int num_linea)
 {
 	stack_t *ojo, *ojo2;
 
-	ojo = *stack;
-	ojo2 = ojo;
-	*stack = ojo->next;
-	(*stack)->prev = NULL;
-	if (num_linea)
-		while (ojo2->next)
-		{
-			ojo2 = ojo2->next;
-		}
-	ojo2->next = ojo;
-	ojo->prev = ojo2;
-	ojo->next = NULL;
+	if (*stack && (*stack)->next)
+	{
+		ojo = *stack;
+		ojo2 = ojo;
+		*stack = ojo->next;
+		(*stack)->prev = NULL;
+		if (num_linea)
+			while (ojo2->next)
+			{
+				ojo2 = ojo2->next;
+			}
+		ojo2->next = ojo;
+		ojo->prev = ojo2;
+		ojo->next = NULL;
+	}
 }
 
 
@@ -58,16 +61,19 @@ void rotr(stack_t **stack, unsigned int num_linea)
 {
 	stack_t *ojo2;
 
-	ojo2 = *stack;
-	if (num_linea)
-		while (ojo2->next)
-		{
-			ojo2 = ojo2->next;
-		}
-	ojo2->next = *stack;
-	ojo2->prev->next = NULL;
-	ojo2->prev = NULL;
-	*stack = ojo2;
+	if (*stack && (*stack)->next)
+	{
+		ojo2 = *stack;
+		if (num_linea)
+			while (ojo2->next)
+			{
+				ojo2 = ojo2->next;
+			}
+		ojo2->next = *stack;
+		ojo2->prev->next = NULL;
+		ojo2->prev = NULL;
+		*stack = ojo2;
+	}
 }
 
 /**
