669cecf28359e73f7d899efee7a537ce73b66d76
diegovivas <dieguitovivas@hotmail.com>
2019-09-29 15:26:13 +0000

    f

--- functions3.c
+++ functions3.c
@@ -133,7 +133,7 @@ void pchar(stack_t **stack, unsigned int num_linea)
 		free(hola.linea);
 		fclose(hola.fil);
 		free_l(stack);
-		dprintf(2, "L%u can't pchar, stack empty\n", num_linea);
+		dprintf(2, "L%u: can't pchar, stack empty\n", num_linea);
 		exit(EXIT_FAILURE);
 	}
 	if (ojo->n > 255 || ojo->n < 0)
